5f42e8762f1c25e4b928a0daaec2489da125319f
Phong <tranloiphong2@gmail.com>
2016-11-03 09:14:34 +0700

    Prepared projecr
    Thiết lập _Layout và Servlet trung tâm

---
+++ VirtualMp3/src/java/Resource/ResourceStrings.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Resource;
+
+/**
+ *
+ * @author Phong
+ */
+public class ResourceStrings {
+    //------------WEB PAGE-------------
+    public static String LAYOUT_PAGE = "_Layout.jsp";
+    public static String INDEX_PAGE = "index.jsp";
+            
+    //------------SERVLET--------------
+    public static String PROCESS_SERVLET = "ProcessServlet";
+    public static String INDEX_SERVLET = "IndexServlet";
+    
+    //------------ACTION---------------
+    
+}
